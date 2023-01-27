26691312c35f87f66942a44e67732710db3806e2
alvin.tan <alvintan95@gmail.com>
2023-01-28 00:44:44 +0800

    add ut for add to bloom filter

--- bloom_test.go
+++ bloom_test.go
@@ -1,9 +1,26 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestBloomFilterAdd(t *testing.T) {
+	bf, _ := NewBloomFilter(100, 0.1)
+	items := []int{1, 2, 3, 10, 20, 30}
+	for _, item := range items {
+		err := bf.Add(item)
+		if err != nil {
+			t.Errorf("Add item returns error :%v", err)
+		}
+	}
+	if bf.n != len(items) {
+		t.Errorf("Expected item count :%v, but got %v", len(items), bf.n)
+	}
+}
 
 func TestBloomFilter(t *testing.T) {
 	bf, _ := NewBloomFilter(100, 0.1)
+	items := []string{"apple", "cherry"}
 
 	testCases := []struct {
 		name     string
@@ -16,15 +33,21 @@ func TestBloomFilter(t *testing.T) {
 		{name: "Test4", value: "date", expected: false},
 	}
 
-	// Add elements to the filter
-	bf.Add("apple")
-	bf.Add("cherry")
+	for _, item := range items {
+		err := bf.Add(item)
+		if err != nil {
+			t.Errorf("Add item returns error :%v", err)
+		}
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			actual, _ := bf.Exist(tc.value)
+			actual, err := bf.Exist(tc.value)
+			if err != nil {
+				t.Errorf("Exist() return error: %v", err)
+			}
 			if actual != tc.expected {
-				t.Errorf("Expected Test() to return %v, but got %v", tc.expected, actual)
+				t.Errorf("Expected Exist() to return %v, but got %v", tc.expected, actual)
 			}
 		})
 	}
